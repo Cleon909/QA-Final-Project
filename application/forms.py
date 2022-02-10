51205c318472c94e37e79c65fd99865bb68b8dab
cleon909 <corcoran909@gmail.com>
2022-02-10 17:10:36 +0000

    started on the body of html for index, removed some attr

--- application/forms.py
+++ application/forms.py
@@ -6,21 +6,18 @@ from models import Academics, Papers, Authors
 
 class SearchDatabaseForm(FlaskForm):
     
-    first_name = SelectField('First name', choices = [])
-    last_name = SelectField('Last name', choices = [])
-    current_instition = SelectField('Current Insitution', choices = [])
+    name = SelectField('Name', choices = [])
+    current_institution = SelectField('Current Insitution', choices = [])
     field_of_study = SelectField('Field of Study', choices = [])
     submit = SubmitField('')
     title = SelectField('Title of Paper', choices = [])
     year_published = SelectField('Year of Publication', choices = [], coerce = int)
-    impact = SelectField('Impact Factor(0 <> 20)', choices = [], coerce = int)
-    field_of_study = SelectField('Field of Study', choices = [])
+    pfield_of_study = SelectField('Field of Study', choices = [])
 
 class UpdateAcademicForm(FlaskForm):
 
     academic_object = SelectField('Academic to Update', choices = [], coerce=int) # should return id of academic object
-    first_name = StringField('First name', validators = [Length(max = 30)])
-    last_name = StringField('Last name', validators = [Length(max = 30)])
+    name = StringField('Name', validators = [Length(max = 50)])
     current_instition = StringField('Current Insitution', validators = [Length(max = 30)])
     field_of_study = StringField('Field of Study', validators = [Length(max = 30)])
     submit = SubmitField('')
@@ -32,14 +29,12 @@ class UpdatePaperForm(FlaskForm):
     paper_object = SelectField('Paper to update', choices = [], coerce = int) 
     title = StringField('Title of Paper', validators = [Length(max = 30)])
     year_published = IntegerField('Year of Publication', validators = [NumberRange(min = 1457, max = 2023)])
-    impact = IntegerField('Impact Factor(0 <> 20)', validators = [NumberRange(min = 0, max = 20)])
     field_of_study = StringField('Field of Study', validators = [Length(max = 30)])
     submit = SubmitField('')
 
 class AddAcademicForm(FlaskForm):
     
-    first_name = StringField('First name', validators = [DataRequired(), Length(max = 30)])
-    last_name = StringField('Last name', validators = [DataRequired(), Length(max = 30)])
+    name = StringField('Name', validators = [DataRequired(), Length(max = 50)])
     current_instition = StringField('Current Insitution', validators = [Length(max = 30)])
     field_of_study = StringField('Field of Study', validators = [Length(max = 30)])
     submit = SubmitField('')
@@ -48,7 +43,6 @@ class AddPaperForm(FlaskForm):
     
     title = StringField('Title of Paper', validators = [Length(max = 30), DataRequired()])
     year_published = IntegerField('Year of Publication', validators = [NumberRange(min = 1457, max = 2023)])
-    impact = IntegerField('Impact Factor(0 <> 20)', validators = [NumberRange(min = 0, max = 20)])
     field_of_study = StringField('Field of Study', validators = [Length(max = 30)])
     authors = SelectField('Authors', choices = [], coerce=int) #should return id of academic object
     submit = SubmitField('')
@@ -56,8 +50,7 @@ class AddPaperForm(FlaskForm):
 class DeleteAcademicForm(FlaskForm):
     #work out here how to allow for partial seraches in each of the fields
     academic_object = SelectField('Academic to Update', choices = [], coerce=int) # should return id of academic object
-    first_name = StringField('First name', validators = [DataRequired(), Length(max = 30)])
-    last_name = StringField('Last name', validators = [DataRequired(), Length(max = 30)])
+    name = StringField('Name', validators = [DataRequired(), Length(max = 50)])
     id = IntegerField('ID', validators = [])
     submit = SubmitField('')
 
--- application/routes.py
+++ application/routes.py
@@ -14,7 +14,39 @@ def index():
     total_number = {'number_of_academics': Academics.query.all.count(),
     'number_of_papers': Papers.query.count()}
     #here put a method to use each field to further filter the results
-    return
+    academic_search_result = Academics.query.all()
+    paper_search_result = Papers.query.all()
+    academics_ = Academics.query.all()
+    for academic in academics_:
+        form.name.choices.append(
+            (academic.id, f"{academic.name}")
+        )
+    for academic in academics_:
+        form.current_institution.choices.append(
+            (academic.id, f"{academic.current_institution}")
+        )
+    for academic in academics_:
+        form.field_of_study.choices.append(
+            (academic.id, f"{academic.field_of_study}")
+        )
+    papers_ = Papers.query.all()
+    for paper in papers_:
+        form.title.choices.append(
+            (paper.id, f"{paper.title}")
+        )
+    for paper in papers_:
+        form.year_published.choices.append(
+            (paper.id, f"{paper.year_published}")
+        )
+    for paper in papers_:
+        form.title.impact.append(
+            (paper.id, f"{paper.impact}")
+        )
+    for paper in papers_:
+        form.title.field_of_study.append(
+            (paper.id, f"{paper.field_of_study}")
+        )
+    return render_template('index.html', form=form, total_number=total_number, academic_search_result=academic_search_result, paper_search_result=paper_search_result )
 
 @app.route('/update_academic', methods = ['GET, POST'])
 def update_academic():
@@ -47,11 +79,10 @@ def update_paper():
 @app.route('/add_academic')
 def add_academic():
     form = AddAcademicForm()
-    first_name = form.first_name.data
-    last_name = form.last_name.data
+    name = form.name.data
     current_institution = form.current_instition.data
     field_of_study = form.field_of_study.data
-    academic = Academics(first_name, last_name, current_institution, field_of_study)
+    academic = Academics(name, current_institution, field_of_study)
     db.session.add(academic)
     db.session.commit()
     return (url_for('index'))
@@ -61,10 +92,9 @@ def add_paper():
     form = AddPaperForm()
     title = form.title.data
     date_published = form.date_published.data
-    impact = form.impact.data
     field_of_study = form.field_of_study.data
     authors = None # this should feed into a method that should add lines ot the child database , with the same paper_id and a new entry for each academic id
-    paper = Papers(title, date_published, impact, field_of_study)
+    paper = Papers(title, date_published, field_of_study)
     db.session.add(paper) #work out how to add authors to child table
     db.commit()
     return
