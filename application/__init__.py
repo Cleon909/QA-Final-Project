3a94a77a3a6665c78179278a3f9318b900e12d44
cleon909 <corcoran909@gmail.com>
2022-02-28 18:05:22 +0000

    another try

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@172.28.0.2:3306/datadb'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
